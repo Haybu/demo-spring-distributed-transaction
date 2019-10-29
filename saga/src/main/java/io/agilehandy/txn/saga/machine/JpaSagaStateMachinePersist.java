a3cab483f775413e7f130e97839849b998058b10
Haytham Mohamed <haybu@hotmail.com>
2019-10-29 13:19:02 -0500

    wip

--- saga/src/main/java/io/agilehandy/txn/saga/machine/JpaSagaStateMachinePersist.java
+++ saga/src/main/java/io/agilehandy/txn/saga/machine/JpaSagaStateMachinePersist.java
@@ -15,17 +15,35 @@
  */
 package io.agilehandy.txn.saga.machine;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+
+import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryo.io.Input;
+import com.esotericsoftware.kryo.io.Output;
+import io.agilehandy.commons.api.jobs.JobEvent;
+import io.agilehandy.commons.api.jobs.JobState;
+import lombok.extern.log4j.Log4j2;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.statemachine.StateMachineContext;
+import org.springframework.statemachine.StateMachinePersist;
+import org.springframework.statemachine.data.jpa.JpaRepositoryStateMachine;
+import org.springframework.statemachine.data.jpa.JpaStateMachineRepository;
+import org.springframework.stereotype.Component;
+
 /**
  * @author Haytham Mohamed
  **/
 
-//@Log4j2
-//@Profile("jpa")
-//@Component
-public class JpaSagaStateMachinePersist {
-		//implements StateMachinePersist<JobState, JobEvent,String> {
+@Log4j2
+@Profile("jpa")
+@Component
+public class JpaSagaStateMachinePersist
+		implements StateMachinePersist<JobState, JobEvent,String> {
 
-	/**
+
 	private final JpaStateMachineRepository stateMachineRepository;
 
 	public JpaSagaStateMachinePersist(JpaStateMachineRepository repository) {
@@ -54,20 +72,26 @@ public class JpaSagaStateMachinePersist {
 		return context;
 	}
 
-	private byte[] convertToBytes(Object object) throws IOException {
-		try (ByteArrayOutputStream bos = new ByteArrayOutputStream();
-			 ObjectOutput out = new ObjectOutputStream(bos)) {
-			out.writeObject(object);
-			return bos.toByteArray();
+	private byte[] convertToBytes(StateMachineContext<JobState, JobEvent> context) throws IOException {
+		try (ByteArrayOutputStream bos = new ByteArrayOutputStream()) {
+			Output output = new Output(bos);
+			Kryo kryo = new Kryo();
+			kryo.writeClassAndObject(output, context);
+			byte[] bytes = output.toBytes();
+			output.close();
+			return bytes;
 		}
 	}
 
-	private Object convertFromBytes(byte[] bytes) throws IOException, ClassNotFoundException {
-		try (ByteArrayInputStream bis = new ByteArrayInputStream(bytes);
-			 ObjectInput in = new ObjectInputStream(bis)) {
-			return in.readObject();
+	private StateMachineContext<JobState, JobEvent> convertFromBytes(byte[] bytes) throws IOException, ClassNotFoundException {
+		try (ByteArrayInputStream bis = new ByteArrayInputStream(bytes)) {
+			Kryo kryo = new Kryo();
+			Input input = new Input(bis);
+			StateMachineContext<JobState, JobEvent> context =
+					(StateMachineContext)kryo.readClassAndObject(input);
+			input.close();
+			return context;
 		}
 	}
-	*/
 
 }
