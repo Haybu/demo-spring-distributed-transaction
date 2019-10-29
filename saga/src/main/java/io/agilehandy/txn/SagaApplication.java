e280dacada15a56ca62e8c1fa274d64008115a99
Haytham Mohamed <haybu@hotmail.com>
2019-10-29 12:54:41 -0500

    wip

--- saga/src/main/java/io/agilehandy/txn/SagaApplication.java
+++ saga/src/main/java/io/agilehandy/txn/SagaApplication.java
@@ -2,8 +2,12 @@ package io.agilehandy.txn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
+@EntityScan
 public class SagaApplication {
 
 	public static void main(String[] args) {
